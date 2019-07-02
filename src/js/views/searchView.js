eaaa99e9b626d41c98463e7539273135608f83b9
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-02 16:36:52 +0100

    Use limit character method with join and reduce

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,4 +1,5 @@
 import { elements } from './base';
+import { cursorTo } from 'readline';
 
 // Don't need `{}` or `return` as ES6 shorthand - one liner
 export const getInput = () => elements.searchInput.value;
@@ -10,6 +11,30 @@ export const clearInput = () => {
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
+
+/*
+'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta'];
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with'];
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato'];
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato'];
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato'];
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle}.join(' ')...`;
+    }
+    return title;
+}
 
 const renderRecipe = recipe => {
     const markup = `
