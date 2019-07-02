6d621904b636d914998d6bdddd0d2a6dae346e2c
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-02 17:16:38 +0100

    Create loader svg

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,3 +3,15 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchResList: document.querySelector('.results__list')
 };
+
+const renderLoader = parent => {
+    const loader = `
+    <div class="loader">
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>
+    `
+    parent.insertAdjacentHTML('afterbegin', loader);
+
+}
