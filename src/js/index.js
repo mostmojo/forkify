41952780a8ce92ebba4c997c30eda2f005b4bc7e
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-02 12:18:39 +0100

    Render data to UI

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,6 @@ const state = {}
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput(); // TODO
-    console.log(query);
 
     if (query) {
         // 2) New search object and add it to state
@@ -26,7 +25,7 @@ const controlSearch = async () => {
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 elements.searchForm.addEventListener('submit', e => {
