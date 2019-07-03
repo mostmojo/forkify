083706ec31bff295e575f4e06821610e052f28e2
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-03 10:46:19 +0100

    Create a function to clear loading and make an object for reusability

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,14 +5,21 @@ export const elements = {
     searchResList: document.querySelector('.results__list')
 };
 
+export const elementStrings = {
+    loader: 'loader'
+};
+
 export const renderLoader = parent => {
     const loader = `
-    <div class="loader">
+    <div class="${elementStrings.loader}">
         <svg>
             <use href="img/icons.svg#icon-cw"></use>
         </svg>
     </div>
     `
     parent.insertAdjacentHTML('afterbegin', loader);
+};
 
-}
+export const clearLoader = () => {
+    const loader = document.querySelector(elementStrings.loader)
+};
