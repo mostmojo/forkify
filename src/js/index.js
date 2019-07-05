c5b92f2f1ccbb4c4f10885c9c526326f8e20e916
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-05 14:41:40 +0100

    Add recipeView in views and import/export from index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
+
 
 /* Global sate of the app
  * - Search object
