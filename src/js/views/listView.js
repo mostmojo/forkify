a7eee366b691da24b4a37149e63b1cbefcd8d034
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-08 13:03:32 +0100

    Add shopping query selector to base, add dynamic values to listView markup

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -1,7 +1,21 @@
 import { elements } from './base';
 
 export const renderItem = item => {
-    const markup = ``
+    const markup = `
+        <li class="shopping__item" data-item-id=${item.id}>
+            <div class="shopping__count">
+                <input type="number" value="${item.count}" step="${item.count}" class="shopping__count--value">
+                <p>${item.unit}</p>
+            </div>
+            <p class="shopping__description">${item.ingredient}</p>
+            <button class="shopping__delete btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-cross"></use>
+                </svg>
+            </button>
+        </li>
+    `;
+    elements.shopping.insertAdjacentElement('beforeend', markup);
 };
 
 export const deleteItem = id => {
