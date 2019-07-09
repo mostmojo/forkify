f8e8165e0b58d267d1e8adccb73e6f83535e3a46
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-07-09 10:03:53 +0100

    Add Like model with like constructor class, addLike and deleteLike method

---
+++ src/js/models/Likes.js
@@ -0,0 +1,16 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, author, img) {
+        const like = { id, title, author, img };
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike(id) {
+
+    }
+}
+
