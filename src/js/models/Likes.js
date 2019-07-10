aa5c2c29d772aaf0853ff2ed692bac11ac87fb10
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-10 23:13:11 +0100

    Add read and persist data functions with stringify and parse

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -32,5 +32,13 @@ export default class Likes {
     persistData() {
         localStorage.setItem('likes', JSON.stringify(this.likes));
     }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'));
+
+        // Restoring likes from the localStorage
+        if (storage) this.likes = storage;
+
+    }
 }
 
