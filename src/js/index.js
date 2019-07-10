de0429bf4a452421ebe881d79340f61c786f8993
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-10 23:17:41 +0100

    Implement restoring liked recipes on page load in controller

--- src/js/index.js
+++ src/js/index.js
@@ -148,10 +148,6 @@ elements.shopping.addEventListener('click', e => {
  * LIKE CONTROLLER
  */
 
- // test
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
  const controlLike = () => {
      if (!state.likes) state.likes = new Likes();
      const currentID = state.recipe.id;
@@ -186,6 +182,20 @@ likesView.toggleLikeMenu(state.likes.getNumLikes());
      }
      likesView.toggleLikeMenu(state.likes.getNumLikes());
  };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    //Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
